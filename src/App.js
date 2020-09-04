3c5e75fe69eb4457803170f4d0cfd439439ec04f
M-Ion <mereutan05@gmail.com>
2020-09-04 22:13:03 +0300

    adding header

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
   // <Route exact(true - this path must be exactly || false) path(string that's equal to the path itself from the current place) component(will be the component that we want to render right).
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={ HomePage } /> 
         <Route path='/shop' component={ ShopPage } /> 
