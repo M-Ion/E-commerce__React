ae3be4fc572fe34273ab3c4fadae920aae5b0ab7
M-Ion <mereutan05@gmail.com>
2020-09-07 16:57:40 +0300

    added add and remove actions for cart items

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -8,3 +8,13 @@ export const addItem = item => ({
     type: CartActionTypes.ADD_ITEM,
     payload: item //In our case we want payload to be the item we're trying to add into our cart items array.
 });
+
+export const clearItemFromCart = item => ({
+    type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+    payload: item
+})
+
+export const removeItem = item => ({
+    type: CartActionTypes.REMOVE_ITEM,
+    payload: item
+})
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,6 @@
 import { CartActionTypes } from './cart.types';
 import { addItemToCart } from './cart.utils';
+import { removeItemFromCart } from './cart.utils';
 
 const INITIAL_STATE = {
     hidden: true,
@@ -17,7 +18,19 @@ const cartReducer = (state = INITIAL_STATE, action) =>{
             return {
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload) // We're doing spreading in all of our array values and then any additional values we add at the end will automatically appear in the very end of this array.
-            }
+            };
+        case CartActionTypes.CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(
+                    cartItem => cartItem.id !== action.payload.id
+                )
+            };
+        case CartActionTypes.REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
+            };
         default:
             return state;
     }
