889e155778f937e79deae9a402cf07c8eaa1328e
M-Ion <mereutan05@gmail.com>
2020-09-08 14:36:17 +0300

    added advaced redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from 'redux'; // Middleware, all it does is it catches the action it console logs it out for us and then it moves it along.
+import { persistStore } from 'redux-persist'; //It allows our browser to actually cache our store depending on certaun configuration.
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
 const middlewares = [ logger ];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store);
